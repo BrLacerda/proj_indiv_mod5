2ee594701af6d857ecf3ab00b449609ff5f46e66
BrLacerda <113605155+BrLacerda@users.noreply.github.com>
2023-02-01 11:20:34 -0300

    Add files via upload
    projeto feito em aula dia 01/02

---
+++ proj_individual_mod-5/projeto.js
@@ -0,0 +1,30 @@
+const readlineSync = require('readline-sync')
+
+
+const lista = [];
+let resp = '';
+
+const adicionarNaLista = (prop) => {
+    lista.unshift(prop)
+};
+
+const ordenar = () => {
+    lista.sort();
+};
+
+const listar = () => {
+    lista.forEach((elemento)=> {console.log(elemento); });
+};
+
+while (true) {
+    resp = readlineSync.question("Digite a propriedade CSS ou 'SAIR' para exibir a listagem e encerrar: ")
+
+    if (resp.toLowerCase() == "sair") {
+        ordenar();
+        listar();
+            break;
+    }
+    else if (resp.toLowerCase() !== "sair" && resp !== "") {
+        adicionarNaLista(resp);
+    }
+}
